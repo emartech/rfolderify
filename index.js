5ba11671194320f85ea8029f98444c5095c5e58e
Matt Brennan <mattyb1000@gmail.com>
2013-05-31 17:22:16 +0100

    passing in options, namely what to use for require and whether to check the file ext (allows use with rfileify)

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 var through = require('through');
 var falafel = require('falafel');
 
-module.exports = function (file) {
+var defaults = {require:"require",}
+
+module.exports = function (file, options) {
     if (/\.json$/.test(file)) return through();
 
     var data = '';
@@ -13,7 +15,7 @@ module.exports = function (file) {
     var varNames = ['__filename', '__dirname', 'path', 'join'];
     var vars = [file, dirname, path, path.join];
     var pending = 0;
-    
+
     var tr = through(write, end);
     return tr;
     
@@ -36,14 +38,20 @@ module.exports = function (file) {
         var output = falafel(data, function (node) {
             if (node.type === 'CallExpression' && node.callee.type === 'Identifier' && node.callee.name === 'rfolder') {
                 ++pending;
-                var arg = node.arguments[0];
-                var t = 'return ' + arg.source();
-                arg = Function(varNames, t).apply(null, vars);
-                listRequireables(arg,function(err,files) {
+                var args = node.arguments;
+                for (var i = 0; i < args.length; i++) {
+                    var t = 'return ' + (args[i]).source();
+                    args[i] = Function(varNames, t).apply(null, vars);
+                }
+                args[1] = args[1] || {};
+                args[1].require = args[1].require || "require";
+                args[1].checkExt = args[1].checkExt != null ? args[1].checkExt : true;
+
+                listRequireables(args[0],args[1].checkExt,function(err,files) {
                     if(err) return tr.emit('error',err);
                     var obj = '{';
                     for(var p in files) if(({}).hasOwnProperty.call(files,p)) {
-                        obj += JSON.stringify(p)+': require('+JSON.stringify(files[p])+'),';
+                        obj += JSON.stringify(p)+': '+args[1].require+'('+JSON.stringify(files[p])+'),';
                     }
                     obj = obj.substr(0,obj.length-1); //remove trailing comma
                     obj += '}';
@@ -56,14 +64,14 @@ module.exports = function (file) {
     }
 };
 
-function listRequireables(dir,cb) {
+function listRequireables(dir,check,cb) {
     fs.readdir(dir,function(err,files) {
         if(err) return cb(err);
         var results = {}, file, base, full, ext;
         for(var i = 0, l = files.length; i < l; ++i) {
             file = files[i];
             ext = path.extname(file);
-            if(ext in require.extensions) { // yup, this works with (coffee|live)ify
+            if(!check || ext in require.extensions) { // yup, this works with (coffee|live)ify
                 base = path.basename(file,ext);
                 full = path.resolve(dir,file);
                 results[base] = full;
